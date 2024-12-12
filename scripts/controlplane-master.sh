c7e51544107a2438be48d45cf27a77ec2d014b31
Mubashar Sadiq <mubashar.sadiq@hotmail.com>
2024-12-12 12:47:07 +0100

    added new wordpress app files

--- scripts/controlplane-master.sh
+++ scripts/controlplane-master.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Setup for Control Plane (Master) servers in AWS with Cilico CNI
+# Setup for Control Plane (Master) servers in AWS with Calico CNI
 
 set -euxo pipefail
 
@@ -13,6 +13,10 @@ MASTER_PRIVATE_IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
 # Pull required images for Kubernetes components
 sudo kubeadm config images pull
 
+# Enable IP forwarding to allow traffic from pods to the internet
+sudo sysctl -w net.ipv4.ip_forward=1
+echo "net.ipv4.ip_forward = 1" | sudo tee -a /etc/sysctl.conf
+
 # Initialize kubeadm
 sudo kubeadm init --control-plane-endpoint="$MASTER_PRIVATE_IP" --apiserver-cert-extra-sans="$MASTER_PRIVATE_IP" --pod-network-cidr="$POD_CIDR" --node-name "$NODENAME"
 
@@ -21,17 +25,24 @@ mkdir -p "$HOME"/.kube
 sudo cp -i /etc/kubernetes/admin.conf "$HOME"/.kube/config
 sudo chown "$(id -u)":"$(id -g)" "$HOME"/.kube/config
 
-# Install Claico Network Plugin Network 
-
+# Install Calico Network Plugin
 kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.29.1/manifests/tigera-operator.yaml
 kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.29.1/manifests/custom-resources.yaml
 
+# Enable NAT for Calico IP pool
+kubectl patch ippool default-ipv4-ippool -p '{"spec":{"natOutgoing":true}}'
+
+# Add iptables rule to allow pod traffic to use NAT
+sudo iptables -t nat -A POSTROUTING -s 192.168.0.0/16 -o eth0 -j MASQUERADE
+
 # Enable kubelet service to start automatically on boot
 sudo systemctl enable kubelet
+
 echo ""
-echo "Kubernetes control plane setup completed successfully with Cilico"
+echo "Kubernetes control plane setup completed successfully with Calico"
 echo ""
 echo ""
+
 # Generate join command for worker nodes
 JOIN_COMMAND=$(kubeadm token create --print-join-command)
 
